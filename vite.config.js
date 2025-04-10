a69da765440e2fb62ac56b1849a595b6de1922df
ashytwilight <45011578+ashytwilight@users.noreply.github.com>
2025-04-10 15:43:10 +0800

    Harrison-20250410

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/tripPlanner/', // 與 repository 名稱完全一致
   plugins: [vue()],
   define: {
     __VUE_PROD_HYDRATION_MISMATCH_DETAILS__: JSON.stringify(false),
